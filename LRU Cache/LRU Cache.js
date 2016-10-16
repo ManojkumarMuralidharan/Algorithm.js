c08a610abbb2cabb231320f59dede728b72f1dd7
Manojkumar Muralidharan <manoj.wolfpack@gmail.com>
2016-10-16 17:03:35 -0400

    Need to fix moveToHead function

--- LRU Cache/LRU Cache.js
+++ LRU Cache/LRU Cache.js
@@ -1,47 +1,145 @@
 function LRUCache(size){
     var capacity;
-    var queueHead = null;
+    var currentQueueSize;
+    var queueHead;
     var hashMap = {};
-    function Node(data){
-        var value = data;
-        var left = null, right =null;
+    var tail;
+    function Node(key,value){
+        this.key = key;
+        this.value = value;
+        this.left = null, this.right =null;
     }
     function init(size){
         console.log('LRU Cache initialized with size:',size);
         capacity = size; 
+        currentQueueSize = 0;
     }
-    function insertNewIntoHead(node){
-        var temp = queueHead;
-        queueHead = node;
-        node.left = null;
-        node.right = temp;
-        temp.left = node;
-    }
+  
     function moveToHead(node){
+        if(queueHead===undefined){
+            queueHead = node;
+            tail = node;
+            return;
+        }
+        //If already in the front don't do anything
+        if(queueHead===node){return;}
+        //if not in the front of the queue
         var temp = queueHead;
         if(node.left){
             node.left.right = node.right;
         }
+        //console.log('moving stuff from queueHead',queueHead);
+        //console.log('moving stuff from temp',temp);
+        node.right = temp;
+        node.left = temp.left;
+        temp.left = node;
         queueHead = node;
-        queueHead.left = null;
-        queueHead.right = temp;
-        temp.left=queueHead;  
+          
     }
-    function get(){
 
+    function get(key){
+        if(hashMap[key]!==undefined){
+            //read the value 
+            hashMap[key].value;
+            //also move it to the front of the queueHead
+            console.log('Getting node:',hashMap[key]);
+            moveToHead(hashMap[key]);
+
+            return hashMap[key].value;
+        }else{
+            return -1;
+        }
     }
-    function set(){
+    function removeTail(){
+        console.log('Tail is:',tail);
+            if(tail===undefined){
+                return;
+            }else if(tail.left===null){
+                hashMap[tail.key]=undefined;
+                queueHead = undefined;
+                tail = undefined;
+            }else{
+                hashMap[tail.key]=undefined;
+                
+                var lastBeforeTail = tail.left;
+                console.log('left of tails is ',lastBeforeTail);
+                lastBeforeTail.right = undefined;
+                tail = lastBeforeTail;
+                console.log('left of tails is ',lastBeforeTail);
+
+            }
+    }
+    function set(key,value){
         
+        //if the key already exist
+        if(hashMap[key]!==undefined){
+           var node = hashMap[key];
+           console.log('Node : ',node,'retrived');
+           moveToHead(node);
+           node.value=value; 
+        } 
+        //else if the key doesnt exist
+        //check if we are at capacity
+        if(capacity === currentQueueSize){
+            //we are at size destroy one
+            console.log('Capacity Exceeded :');
+            removeTail();
+            currentQueueSize--;
+            console.log('After removing :');
+            printSize();
+        }
+        if(hashMap[key]===undefined){
+            var node = new Node(key,value);
+            console.log('New node :',node,' created');
+            moveToHead(node);
+            hashMap[key] = node;
+            currentQueueSize++;
+        }
+        
+    }
+    function printSize(){
+        console.log('currentQueueSize:',currentQueueSize);
+        var temphead = queueHead; 
+        var printString = '';
+        console.log('-----Queue------');
+        while(temphead!=null){
+            printString = printString + '| Key ['+temphead.key+']:'+temphead.value+', left:'+temphead.left+', right:'+temphead.right+' | --->';
+            if(temphead.left){
+                printString = printString + 'left key:' + temphead.left.key;
+            }
+            if(temphead.right){
+                printString = printString + 'right key:' + temphead.right.key;
+            }
+            temphead=temphead.right;
+        }
+        console.log('QueueHead --->'+printString+'Null');
+        
+        console.log('-----HashMap------');
+    //    for(var i in hashMap){
+    //        console.log('key:',i,'value:',hashMap[i]);
+    //    }
+     //  console.log('tail is ',tail);
     }
 
     init(size);
     return {
         get : get,
         set : set,
+        printSize : printSize,
         size : capacity
     }
 };
 
-var LRUCache1 = new LRUCache(10);
-
+var LRUCache1 = new LRUCache(4);
+LRUCache1.set(7,'m');
+LRUCache1.printSize();
+LRUCache1.set(8,'a');
+LRUCache1.printSize();
+LRUCache1.set(5,'p');
+LRUCache1.printSize();
+LRUCache1.set(6,'k');
+LRUCache1.printSize();
+LRUCache1.get(8);
+LRUCache1.set(1,'t');
+LRUCache1.printSize();
 
